9f8160de19e1d64c49689330030fe81da4b5433b
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2022-07-09 13:07:25 -0300

    section-11

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,7 @@
-import Head from 'next/head'
-
-import Layout from '../components/layout/layout'
-import { NotificationContextProvider } from '../store/notification-context'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <NotificationContextProvider>
-      <Layout>
-        <Head>
-          <title>Next Events</title>
-          <meta name='description' content='NextJS Events' />
-          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-        </Head>
-        <Component {...pageProps} />
-      </Layout>
-    </NotificationContextProvider>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
