daad8d940dde5557d62266acffeff2b7b7644f3a
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2022-07-08 22:32:11 -0300

    section-10

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,12 +1,27 @@
-import MainHeader from "./main-header"
+import { useContext } from 'react'
+
+import MainHeader from './main-header'
+import Notification from '../ui/notification'
+import NotificationContext from '../../store/notification-context'
 
 function Layout ({ children }) {
+  const notificationCtx = useContext(NotificationContext)
+
+  const activeNotification = notificationCtx.notification
+
   return (
     <>
       <MainHeader />
       <main>
         {children}
       </main>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </>
   )
 }
--- components/ui/notification.js
+++ components/ui/notification.js
@@ -1,35 +1,33 @@
-import { useContext } from 'react';
+import { useContext } from 'react'
 
-import classes from './notification.module.css';
-import NotificationContext from '../../store/notification-context';
+import classes from './notification.module.css'
+import NotificationContext from '../../store/notification-context'
 
-function Notification(props) {
-  const notificationCtx = useContext(NotificationContext);
+function Notification({ title, message, status }) {
+  const notificationCtx = useContext(NotificationContext)
 
-  const { title, message, status } = props;
-
-  let statusClasses = '';
+  let statusClasses = ''
 
   if (status === 'success') {
-    statusClasses = classes.success;
+    statusClasses = classes.success
   }
 
   if (status === 'error') {
-    statusClasses = classes.error;
+    statusClasses = classes.error
   }
 
   if (status === 'pending') {
-    statusClasses = classes.pending;
+    statusClasses = classes.pending
   }
 
-  const activeClasses = `${classes.notification} ${statusClasses}`;
+  const activeClasses = `${classes.notification} ${statusClasses}`
 
   return (
     <div className={activeClasses} onClick={notificationCtx.hideNotification}>
       <h2>{title}</h2>
       <p>{message}</p>
     </div>
-  );
+  )
 }
 
-export default Notification;
+export default Notification
