c6dd10c5d51c19b1cf9ecf2a3b3180c6a4d92aee
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2022-07-15 16:42:44 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    reactStrictMode: false,
-    swcMinify: true
-  }
+  reactStrictMode: false,
+  swcMinify: true
 }
 
 module.exports = nextConfig
