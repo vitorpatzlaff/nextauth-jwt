3d26a6a74fad4c06b00c8c4a337c0271f1fbc231
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2022-07-15 17:00:51 -0300

    updade next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     mongodb_username: 'vitorpatzlaff',
     mongodb_password: 'nextmongo',
     mongodb_clustername: 'cluster0',
-    mongodb_database: 'my-site-dev'
+    mongodb_database: 'test'
   }
 }
 
