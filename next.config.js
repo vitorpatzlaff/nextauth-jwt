520008342317a47c9540b57b54a7c16a54302cd0
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2022-07-15 16:59:59 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,13 +2,7 @@
 
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify: true,
-  env: {
-    mongodb_username: 'vitorpatzlaff',
-    mongodb_password: 'nextmongo',
-    mongodb_clustername: 'cluster0',
-    mongodb_database: 'my-site-dev'
-  }
+  swcMinify: true
 }
 
 module.exports = nextConfig
