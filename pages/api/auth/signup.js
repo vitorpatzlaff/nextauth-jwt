632f9c8765855a969b027c7bbb4d7d72de5918df
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2022-07-16 14:48:42 -0300

    section-13

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,12 @@
+function handler () {
+
+}
+
+export default handler
+
+/*
+  mongodb_username: 'vitorpatzlaff',
+  mongodb_password: 'nextmongo',
+  mongodb_clustername: 'cluster0',
+  mongodb_database: ''
+*/
